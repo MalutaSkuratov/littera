ab6e2933b71436222d919b166da531c5b201e7f8
Alexandr <narvyshalexandr@gmail.com>
2022-08-27 20:01:37 +0300

    Database

--- src/main/java/ru/littera/message/controllers/MessageController.java
+++ src/main/java/ru/littera/message/controllers/MessageController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.littera.message.models.Message;
 
@@ -26,6 +27,13 @@ public class MessageController {
         message.setId(++nextId);
         messages.add(message);
         return "redirect:/";
+    }
+
+    @PostMapping("/messages/delete/{id}")
+    private String deleteMessage(@PathVariable Long id){
+        messages.removeIf(message -> message.getId().equals(id));
+        return "redirect:/";
+
     }
 
 
--- src/main/java/ru/littera/message/models/Message.java
+++ src/main/java/ru/littera/message/models/Message.java
@@ -3,9 +3,15 @@ package ru.littera.message.models;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "messages")
 public class Message {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String message;
